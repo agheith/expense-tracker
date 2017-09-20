62cc7ee52c819f7a65e6ffc6210ae935131aaa79
agheith <amerghth@gmail.com>
2017-09-19 19:58:54 -0700

    Set up react-router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter'
+import { BrowserRouter } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>My boilerplate</p>, document.getElementById('app'));
+
+
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
